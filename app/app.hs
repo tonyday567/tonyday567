2ce303948e1c5e7257ac6573500d226bafd467cf
Tony Day <tonyday567@gmail.com>
2024-12-16 10:41:56 +1000

    yml

--- app/app.hs
+++ app/app.hs
@@ -36,7 +36,7 @@ row user (repo, ci) =
 |]
   where
     citext :: String
-    citext = if ci == CI then [i|[![CI](https://github.com/#{user}/#{repo}/actions/workflows/haskell-ci.yaml/badge.svg?branch=main)](https://github.com/#{user}/#{repo}/actions)|] else mempty
+    citext = if ci == CI then [i|[![CI](https://github.com/#{user}/#{repo}/actions/workflows/haskell-ci.yml/badge.svg?branch=main)](https://github.com/#{user}/#{repo}/actions)|] else mempty
 
 emacsRow :: String -> String -> String
 emacsRow user repo =
